5fa95c0d9e2785d545a6867cca1e207c766b7f09
elimora <elimorameta80@gmail.com>
2022-08-13 17:37:04 -0400

    Initializing login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmailVerificationComponent } from './components/email-verification/email-verification.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'user-register', component: UserRegisterComponent },
+  { path: 'email-verification', component: EmailVerificationComponent },
+  { path: 'recover-password', component: UserRegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { EmailVerificationComponent } from './components/email-verification/email-verification.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UserRegisterComponent,
+    EmailVerificationComponent,
+    RecoverPasswordComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
